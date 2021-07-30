7d0edfe10303358fcbaa318df90d68c5060e66ed
MiguelAngelMMendoza <miguel5963@ciencias.unam.mx>
2021-07-30 16:15:09 -0500

    Proyecto terminado, se corrigieron algunos bugs

--- src/app/components/galeria.component.ts
+++ src/app/components/galeria.component.ts
@@ -2,29 +2,17 @@ import { Component } from '@angular/core';
 
 @Component({
 	selector: 'galeria',
-<<<<<<< HEAD
 	templateUrl: '../views/galeria.html',
 	styleUrls: ['../css/galeria.component.css']
-=======
-	templateUrl: '../views/galeria.html'
->>>>>>> 6be5d378c104259df056e1c3756b179b03b973e7
 })
 export class GaleriaComponent {
 	public titulo:string;
 
 	constructor(){
-<<<<<<< HEAD
 		this.titulo = "Galería"
 	}
 
 	ngOnInit(){
 		console.log("Componente galeria.component.ts cargado");
-=======
-		this.titulo = "Natación"
-	}
-
-	ngOnInit(){
-		console.log("Componente natacion.component.ts cargado");
->>>>>>> 6be5d378c104259df056e1c3756b179b03b973e7
 	}
 }
--- src/app/components/quienes-somos.component.ts
+++ src/app/components/quienes-somos.component.ts
@@ -2,12 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
 	selector: 'quienes-somos',
-<<<<<<< HEAD
 	templateUrl: '../views/quienes-somos.html',
 	styleUrls: ['../css/quienes-somos.component.css']
-=======
-	templateUrl: '../views/quienes-somos.html'
->>>>>>> 6be5d378c104259df056e1c3756b179b03b973e7
 })
 export class QuienesSomosComponent {
 	public titulo:string;
